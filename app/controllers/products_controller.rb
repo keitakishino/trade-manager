b824d891d3c6f4022cccc7e6a0aaee189ac6d806
Keita Kishino <kishino@netlab.jp>
2023-09-07 21:59:47 +0900

    feat: バインダー編集画面

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[edit update show destroy]
   def index
     @products = Product.all
   end
@@ -18,7 +19,30 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @product.update(update_product_params)
+     flash.now.notice = "更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.delete
+    flash.now.notice = "削除しました。"
+  end
+
   private
+
+  def set_product
+    @product = Product.find params[:id]
+  end
 
   def name_params
     params.permit(:name)
@@ -26,5 +50,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :num, :price)
+  end
+
+  def update_product_params
+    params.require(:product).permit(:num, :price)
   end
 end
